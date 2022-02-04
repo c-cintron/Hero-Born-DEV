9b0816f7c88d3f2f5bed2137b16c09f3ca047d68
c-cintron <CINTROC3@my.erau.edu>
2022-02-04 00:40:31 -0700

    Chapter 7

--- Hero Born V2/Assets/Scripts/CameraBehavior.cs
+++ Hero Born V2/Assets/Scripts/CameraBehavior.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour
 {
+    public Vector3 camOffset = new Vector3(0f, 1.2f, -2.6f);
+
+    private Transform target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.Find("Player").transform;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        this.transform.position = target.TransformPoint(camOffset);
+
+        this.transform.LookAt(target);
     }
 }
--- Hero Born V2/Assets/Scripts/PlayerBehavior.cs
+++ Hero Born V2/Assets/Scripts/PlayerBehavior.cs
@@ -10,10 +10,11 @@ public class PlayerBehavior : MonoBehaviour
     private float vInput;
     private float hInput;
 
+    private Rigidbody _rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,8 +22,19 @@ public class PlayerBehavior : MonoBehaviour
     {
         vInput = Input.GetAxis("Vertical") * moveSpeed;
         hInput = Input.GetAxis("Horizontal") * rotateSpeed;
-
+        /*
         this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
         this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+        */ 
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 rotation = Vector3.up * hInput;
+
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+        _rb.MoveRotation(_rb.rotation * angleRot);
     }
 }
