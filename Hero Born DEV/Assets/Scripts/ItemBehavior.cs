74e3fabce88e2f624bb4188006ac162b109c1cb1
c-cintron <98188001+c-cintron@users.noreply.github.com>
2022-02-09 13:13:24 -0700

    update

---
+++ Hero Born DEV/Assets/Scripts/ItemBehavior.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBehavior : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(this.transform.parent.gameObject);
+
+            if (this.name == "Health_Pickup")
+            {
+                Debug.Log("Health Item collected!");
+            } else if (this.name == "Strength_Pickup")
+            {
+                Debug.Log("Strength Item collected!");
+            } else if (this.name == "Speed_Pickup")
+            {
+                Debug.Log("Speed Item collected!");
+            } else if (this.name == "Pogo_Pickup")
+            {
+                Debug.Log("Pogo Item collected!");
+            }
+        }
+    }
+}
